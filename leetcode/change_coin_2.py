a040c7fcf4083197d630d4dcc949e1fbe889a997
santhoshmohan05 <you@example.com>
2022-10-07 22:47:52 +0800

    Added code

---
+++ leetcode/change_coin_2.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/coin-change-ii/
+
+class Solution:    
+    def change(self, amount: int, coins: List[int]) -> int:
+        change = [0]*(amount+1)
+        change[0] = 1
+        for i in range(len(coins)):
+            for j in range(amount+1):
+                change[j] += change[j - coins[i]] if j - coins[i] >= 0 else 0
+        return change[-1]
+            
+        
