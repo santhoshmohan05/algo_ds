e35b4c3ac058d88d101e52f9da2da7285dc92ee7
santhoshmohan05 <santhoshmohan05@gmail.com>
2025-03-02 20:30:23 -0500

    Added a new problem

---
+++ leetcode/python/largest_rectangle_historgam.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        for i in range(len(heights)):
+            while len(stack) > 0 and heights[stack[-1]] > heights[i]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1 if len(stack) > 0 else i
+                max_area = max(max_area,h*w)
+            stack.append(i)
+        while len(stack) > 0:
+            h = heights[stack.pop()]
+            if len(stack) > 0:
+                w = len(heights) - stack[-1] - 1
+            else:
+                w = len(heights)
+            max_area = max(max_area,h*w)
+        return max_area
+
+    def largestRectangleAreaBruteForce(self, heights: List[int]) -> int:
+        max_rectangle = 0
+        for i in range(len(heights)):
+            for j in range(i, -1, -1):
+                rect_size = min(heights[j:i+1])*(i-j+1)
+                if max_rectangle < rect_size:
+                    max_rectangle = rect_size
+        return max_rectangle
+
+        
