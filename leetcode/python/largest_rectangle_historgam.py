c6a5cfb8f3efced542db037422b4ae5be2dd15f1
santhoshmohan05 <santhoshmohan05@gmail.com>
2025-03-02 20:31:30 -0500

    Added a new problem

--- leetcode/python/largest_rectangle_historgam.py
+++ leetcode/python/largest_rectangle_historgam.py
@@ -1,3 +1,8 @@
+"""
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+"""
+
 from typing import List
 
 class Solution:
